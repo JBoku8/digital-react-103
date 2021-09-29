2d46837c32c1490df887ce73fe0c146667d84380
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-09-30 03:46:25 +0400

    first-lecture-introduction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Greeting from "./Greeting";
+
+import "./App.css";
 
 function App() {
+  const h1 = React.createElement(
+    "h1",
+    {
+      className: "example-class",
+    },
+    "React.createElement example"
+  );
+
+  const myText = "Demo Text";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{myText}</h1>
+        {h1}
+        <Greeting />
       </header>
     </div>
   );
---
+++ src/Greeting.js
@@ -0,0 +1,9 @@
+function Greeting() {
+  return (
+    <div className="greeting-message">
+      <h2>Greeting Component</h2>
+    </div>
+  );
+}
+
+export default Greeting;
